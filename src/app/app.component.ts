8231cb521adc1672b0b1ac74e5b832d4e294d2fd
qudwns017 <qudwns017@naver.com>
2021-12-06 19:27:47 +0900

    2021-2nd Final Exam

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Register', url: '/register', icon: 'person-add' },
+    { title: 'Login', url: '/login', icon: 'enter' },
+    { title: 'Dashboard', url: '/dashboard', icon: 'card' },
+    { title: 'Profile', url: '/profile', icon: 'body' },
+    { title: 'Live Chat', url: '/chat', icon: 'chatbubbles' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() { }
+}
